b1c38d4413bc124f7c190554bc1f93680fac225c
Artem <artemlyksa@gmail.com>
2019-09-21 10:59:21 +0300

    Added handling of GET request and returning of data

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const express = require('express');
 const app = express();
 
+const courses = [
+    { id: 1, name: 'course1' },
+    { id: 2, name: 'course2' },
+    { id: 3, name: 'course3' },
+    { id: 4, name: 'course4' },
+];
+
 app.get('/', (req, res) => {
     res.send('Hello world');
 });
 
 app.get('/api/courses', (req, res) => {
-    res.send([1, 2, 3]);
+    res.send(courses);
 });
 
 app.get('/api/courses/:id', (req, res) => {
-    // res.send(req.params.id);
-    res.send(req.query);
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('The course with the given ID was not found');
+    res.send(course);
 });
 
 // PORT
