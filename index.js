d363a3a58312717cc1266ec4bef45fdbfcd255de
Artem <artemlyksa@gmail.com>
2019-09-26 13:43:52 +0300

    Added urlencoded() and static() middleware functions

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true })); //key=value&key=value
+app.use(express.static('public'));
 
 app.use(logger);
 
